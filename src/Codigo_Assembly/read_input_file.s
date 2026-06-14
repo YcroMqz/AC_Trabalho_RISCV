743657c0a6be55cce21e631db9f94a6f254bad68
YcroMqz <icayw12@gmail.com>
2026-06-14 16:32:25 -0300

    feat: Adicionando comentarios

--- src/Codigo_Assembly/read_input_file.s
+++ src/Codigo_Assembly/read_input_file.s
@@ -3,14 +3,16 @@
 .text
 .globl read_input_file
 read_input_file:
-
+    # syscall open file
     la a0, filename
     li a1, 0
     li a7, 1024
     ecall
 
+    # salva o retorno do syscall open file (descritor do arquivo)
     mv t0, a0
 
+    # realiza leitura do arquivo 
     la a1, string
     li a2, 499
     li a7, 63
@@ -20,6 +22,7 @@ read_input_file:
     add t1, t1, a0
     sb zero, 0(t1)
 
+    # syscall close file
     mv a0, t0
     li a7, 57
     ecall
